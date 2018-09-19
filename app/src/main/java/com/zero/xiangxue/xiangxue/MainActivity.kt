bcccfbc2d2a634624b1d3d90e5e8062a058c8b5d
fanzhang <fan_0723@qq.com>
2018-09-19 22:03:31 +0800

    享学课程体系

---
+++ app/src/main/java/com/zero/xiangxue/xiangxue/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.zero.xiangxue.xiangxue
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.zero.xiangxue.handlerdemo.HandlerDemoActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
---
+++ handlerdemo/src/main/java/com/zero/xiangxue/handlerdemo/HandlerDemoActivity.java
@@ -0,0 +1,18 @@
+package com.zero.xiangxue.handlerdemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by wanghuajun on 2018/9/19.
+ */
+
+public class HandlerDemoActivity extends Activity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
