b005d4094597abdac58ab4ba1296f1007e459891
Zero <fan_0723@qq.com>
2018-12-06 18:20:11 +0800

    javapoet练习

--- source/javapoet/JavaPoetDemo/processors/src/main/java/com/enjoy/zero/processors/JavapoetTest.java
+++ source/javapoet/JavaPoetDemo/processors/src/main/java/com/enjoy/zero/processors/JavapoetTest.java
@@ -1,7 +1,7 @@
 package com.enjoy.zero.processors;
 
 
-
+import com.enjoy.zero.bean.Human;
 import com.enjoy.zero.javapoet.JavaFile;
 import com.enjoy.zero.javapoet.MethodSpec;
 import com.enjoy.zero.javapoet.TypeSpec;
@@ -22,31 +22,81 @@ public class JavapoetTest {
 
     public static void main(String[] args) {
 
-        testHelloWorld();
+//        testHelloWorld();
+        testControlFlow();
     }
 
     public static void testHelloWorld() {
         //第一步 生成main函数
         MethodSpec main = MethodSpec.methodBuilder("main")
-                .addModifiers(Modifier.PUBLIC,Modifier.STATIC)
+                .addModifiers(Modifier.PUBLIC, Modifier.STATIC)
                 .returns(void.class)
-                .addParameter(String[].class,"args")
-                .addStatement("$T.out.println($S)",System.class,"Hello, JavaPoet!")
+                .addParameter(String[].class, "args")
+                .addStatement("$T.out.println($S)", System.class, "Hello, JavaPoet!")
                 .build();
         //第二步 生成类
         TypeSpec helloWorld = TypeSpec.classBuilder("HelloWorld")
-                .addModifiers(Modifier.PUBLIC,Modifier.FINAL)
+                .addModifiers(Modifier.PUBLIC, Modifier.FINAL)
                 .addMethod(main)
                 .build();
         //第三步 生成java文件对象
-        JavaFile javaFile = JavaFile.builder("com.zero.helloworld",helloWorld).build();
+        JavaFile javaFile = JavaFile.builder("com.zero.helloworld", helloWorld).build();
         //第四步 输出到控制台
         try {
             javaFile.writeTo(System.out);
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-
     }
+
+    public static void testControlFlow() {
+        MethodSpec main = MethodSpec.methodBuilder("main")
+                .addCode(""
+                        + "int total = 0;\n"
+                        + "for (int i = 0; i < 10; i++){\n"
+                        + " total += i;\n"
+                        + "}\n")
+                .build();
+
+        MethodSpec main1 = MethodSpec.methodBuilder("main1")
+                .addStatement("int total = 0;")
+                .beginControlFlow("for (int i = 0; i < 10; i++)")
+                .addStatement("total += i")
+                .endControlFlow()
+                .build();
+
+        MethodSpec display = MethodSpec.methodBuilder("display")
+                .addStatement("Human human = new Human();",Human.class)
+                .addStatement("human.name = \"fanzhang\"")
+                .beginControlFlow("for (int i = 0; i < name.length; i++)")
+                .addStatement("$T.out.println($S)", System.class, "name.charAt(i)")
+                .endControlFlow()
+                .build();
+
+        TypeSpec typeSpec = TypeSpec.classBuilder("HelloWorld")
+                .addModifiers(Modifier.PUBLIC, Modifier.FINAL)
+                .addMethod(main)
+                .addMethod(main1)
+                .addMethod(display)
+                .build();
+        JavaFile javaFile = JavaFile.builder("com.zero.controlflow", typeSpec).build();
+        try {
+            javaFile.writeTo(System.out);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private MethodSpec computeRange(String name, int from, int to, String op) {
+        return MethodSpec.methodBuilder(name)
+                .returns(int.class)
+                .addStatement("int result = 1;")
+                .beginControlFlow("for (int i = " + from + "; i < " + to + "; i++" + ")")
+                .addStatement("result = result" + op + "i")
+                .endControlFlow()
+                .addStatement("return result")
+                .build();
+    }
+
+
 }
