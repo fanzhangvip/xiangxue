a9ac85cc9b7f2f312dc1dfff2330d2a31df54041
zero <fan_0723@qq.com>
2019-03-26 16:32:26 +0800

    一堆flutter学习资料，从其他地方来的，非原创

---
+++ source/notebook/example/flutter_provide/README.md
@@ -0,0 +1,18 @@
+# flutter_provide
+Provide是Google官方推出的状态管理模式，更多请访问：https://github.com/google/flutter-provide
+
+## 样例
+![](https://user-gold-cdn.xitu.io/2019/2/20/1690aee3246a13ec?w=371&h=742&f=gif&s=316988)
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://flutter.io/docs/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://flutter.io/docs/cookbook)
+
+For help getting started with Flutter, view our 
+[online documentation](https://flutter.io/docs), which offers tutorials, 
+samples, guidance on mobile development, and a full API reference.
---
+++ source/notebook/ios/Classes/NotebookPlugin.m
@@ -0,0 +1,20 @@
+#import "NotebookPlugin.h"
+
+@implementation NotebookPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"notebook"
+            binaryMessenger:[registrar messenger]];
+  NotebookPlugin* instance = [[NotebookPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
